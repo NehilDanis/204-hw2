e9bd7dd0ba53f07425ebf3d4b313497f39a4de5d
nehil <nehild@outlook.com>
2016-03-07 12:45:30 +0200

    deneme

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 	public static void main(String [] args){
 		//ReadInput.readList(args[1]);//Here we added all the customers to the system.
 		ReadInput.readCommand(args[0]);//Here decided what to do with all these customer informations.
+		System.out.println();
 	}
 }
